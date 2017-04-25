6dae2b43ad5a737a8608f8d9a7756caf80022455
robtb <robert.t.bisson@gmail.com>
2017-04-25 18:54:13 -0400

    2nd commit

--- wu_utilities.py
+++ wu_utilities.py
@@ -7,13 +7,13 @@ from api_key import *
 def wu_history_url(city, state, day):
     # must use correct capitalization for city/state (City, ST)
     # YYYYMMDD
-    url = 'http://api.wunderground.com/api/' + api_key + '/history_' + day + '/q/' + state + '/' + city + '.json'
+    url = 'http://api.wunderground.com/api/' + wu_api_key + '/history_' + day + '/q/' + state + '/' + city + '.json'
 
     return url
 
 
 def wu_forecast_url(city, state):
-    url = 'http://api.wunderground.com/api/' + api_key + '/forecast/q/' + state + '/' + city + '.json'
+    url = 'http://api.wunderground.com/api/' + wu_api_key + '/forecast/q/' + state + '/' + city + '.json'
 
     return url
 
@@ -30,14 +30,33 @@ def date_gen(year, start_month, start_day, end_month, end_day):
     return formatted_dates
 
 
+def date_format(x):
+    year = x[0:4]
+    if x[4] == '0':
+        month = x[5]
+    else:
+        month = x[4:6]
+
+    if x[6] == '0':
+        day = x[7]
+    else:
+        day = x[6:8]
+    new_date = '{}/{}/{}'.format(month, day, year)
+    return new_date
+
 def wu_actual_observations(city, state, focus_day):
     weather_data = (requests.get(wu_history_url(city, state, focus_day))).json()
     # access the observed max and min temps using dict keys and list indices
     max_temp = weather_data['history']['dailysummary'][0]['maxtempi']
     min_temp = weather_data['history']['dailysummary'][0]['mintempi']
     precip = weather_data['history']['dailysummary'][0]['precipi']
+    # need to replace T (trace) in precip with 0 for stat comparison
 
-    return focus_day, max_temp, min_temp, precip
+
+    #formate the date to be able to easily compare the resultant with the three_day_forecast csv
+    proper_date = date_format(focus_day)
+
+    return proper_date, max_temp, min_temp, precip
 
 
 
